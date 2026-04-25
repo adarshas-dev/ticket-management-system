df188d8830a6ef34ebbbc98e43de1c8fc3085a3a
adarshas-dev <adarshasokakumar0@gmail.com>
2026-04-25 20:46:39 +0530

    Updated frontend API URL for production

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://ticket-management-system-3jgy.onrender.com",
 });
 
 // Request interceptor
